25cd38a5a68e1d308c08d4c729b262bba1efa2b8
oelizondo <oscarmarcelo95@gmail.com>
2017-06-24 14:18:36 -0500

    finishes testing setup

--- db/index.js
+++ db/index.js
@@ -1,8 +1,4 @@
 const Sequelize = require('sequelize')
 const sequelize = new Sequelize(process.env.DATABASE_URL)
 
-sequelize.sync({force: true}).then(() => {
-  console.log('Setup')  
-})
-
 module.exports = sequelize
--- test/index.js
+++ test/index.js
@@ -2,7 +2,5 @@ const app = require('../app')
 const test = require('./api_test')
 const sequelize = require('../db')
 
-sequelize.authenticate().then(() => {
-  test(app)
-}).catch(error => console.log(error))
-
+test(app)
+sequelize.close()
