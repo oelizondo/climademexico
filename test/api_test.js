448d596fcb32704a64fad01ef257898f0b57acdd
oelizondo <oscarmarcelo95@gmail.com>
2017-06-24 15:59:30 -0500

    testing suite

--- test/api_test.js
+++ test/api_test.js
@@ -12,4 +12,24 @@ module.exports = (app) => {
       assert.end()
     })
   })
+  test('Correct api call to a city', (assert) => {
+    request(app)
+    .get('/api/cities/monterrey')
+    .expect(200)
+    .end((err, res) => {
+      const body = res.body
+      assert.same([], body)
+      assert.end()
+    })
+  })
+  test('Correct api call to a city in a year', (assert) => {
+    request(app)
+    .get('/api/cities/monterrey/1977')
+    .expect(200)
+    .end((err, res) => {
+      const body = res.body
+      assert.same([], body)
+      assert.end()
+    })
+  })
 }
